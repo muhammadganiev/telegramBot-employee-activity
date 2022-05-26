5b5c3f63374d108ee4665eb921fb1725f162de62
muhammadganiev <mukhammad.ganiev@mail.ru>
2022-05-26 15:18:34 +0400

    replit code sender with user and admin

--- telebot.py
+++ telebot.py
@@ -1,16 +1,36 @@
-import telebot
+import telegram_send
+import os
+import telebot, time
+from telebot import types
 
-TOKEN = '5328372677:AAEdTWisnVtVOoleO-pojxmEBXCZdRe_CTw'
 
-bot = telebot.TeleBot(TOKEN)
-def echo_messages(*messages):
-    for m in messages:
-        chatid = m.chat.id
-        if m.content_type == 'text':
-            text = m.text
-            bot.send_message(chatid, text)
-bot.set_update_listener(echo_messages)
-bot.polling()
+bot = telebot.TeleBot(os.environ['API_KEY'])
+tconv = lambda x: time.strftime("%H:%M:%S %d.%m.%Y", time.localtime(x))
 
-while True: # Don't let the main Thread end.
-    pass
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+  keyboard = types.InlineKeyboardMarkup()
+
+  keyboard.row(
+    types.InlineKeyboardButton(text='Отправить локацию', callback_data="location"))
+  
+  if call.data == "otkrit":
+    bot.send_message(call.message.chat.id, "Отправьте локацию для подтверждения смены", reply_markup = keyboard)
+    telegram_send.send(messages = [f'{call.message.chat.first_name} открыл смену в {tconv(call.message.date)}']);
+  if call.data == "location":
+    bot.send_message(call.message.chat.id, 'Вы удачно отметились, хорошего рабочего дня')
+@bot.message_handler()
+def user(message):
+  keyboard = types.InlineKeyboardMarkup()
+
+  keyboard.row(
+    types.InlineKeyboardButton(text='Открыть смену', callback_data="otkrit"),
+    types.InlineKeyboardButton(text='Закрыть смену', callback_data="zakrit"))
+  if message.text.lower() == "отметиться":
+    bot.send_message(message.chat.id, "Отметьтесь перед началом рабочего дня :)", reply_markup = keyboard)
+    
+  #@bot.message_handler()
+   # def admin(message):
+  #   if call.data == "true":
+   #     telegram_send.send(messages = [f'{message.from_user.first_name} открыл смену в {tconv(message.date)}']);
+bot.polling(none_stop=True)
